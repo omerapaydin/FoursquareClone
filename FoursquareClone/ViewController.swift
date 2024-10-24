9490addd31710772f842b156e0f073e80f410415
Ömer Apaydın <omerapaydin90@gmail.com>
2024-10-24 17:24:10 +0300

    Initial Commit

---
+++ FoursquareClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by Ömer on 24.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FoursquareCloneTests/FoursquareCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoursquareCloneTests.swift
+//  FoursquareCloneTests
+//
+//  Created by Ömer on 24.10.2024.
+//
+
+import Testing
+@testable import FoursquareClone
+
+struct FoursquareCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
